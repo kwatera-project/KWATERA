e71f616f9ec1ec8189ef28b5e1ca0a6004f495c1
Nadzeya Silchankava <sinadzeya@Nadzeyas-MacBook-Pro.local>
2026-04-15 15:31:53 +0200

    Fix spotless formatting v2

--- services/reservation-service/src/test/java/io/github/kwatera_project/kwatera/reservation_service/ReservationServiceApplicationTests.java
+++ services/reservation-service/src/test/java/io/github/kwatera_project/kwatera/reservation_service/ReservationServiceApplicationTests.java
@@ -3,6 +3,4 @@ package io.github.kwatera_project.kwatera.reservation_service;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ReservationServiceApplicationTests {
-
-}
+class ReservationServiceApplicationTests {}
