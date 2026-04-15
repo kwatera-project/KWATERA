94ad9833b5f6703ed39a716758dc5ece3cab6f8d
Nadzeya Silchankava <sinadzeya@Nadzeyas-MacBook-Pro.local>
2026-04-15 15:29:26 +0200

    Delete empty test

--- services/reservation-service/src/test/java/io/github/kwatera_project/kwatera/reservation_service/ReservationServiceApplicationTests.java
+++ services/reservation-service/src/test/java/io/github/kwatera_project/kwatera/reservation_service/ReservationServiceApplicationTests.java
@@ -1,11 +1,8 @@
 package io.github.kwatera_project.kwatera.reservation_service;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ReservationServiceApplicationTests {
 
-  @Test
-  void contextLoads() {}
 }
