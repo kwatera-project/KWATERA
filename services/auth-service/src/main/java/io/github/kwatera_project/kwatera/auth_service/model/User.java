0dc5672c0dfe6f79cbcf864988cf4e8a2b0e1f0f
Nadzeya Silchankava <sinadzeya@Nadzeyas-MacBook-Pro.local>
2026-04-22 17:35:29 +0200

    Fix formatting

--- services/auth-service/src/main/java/io/github/kwatera_project/kwatera/auth_service/model/User.java
+++ services/auth-service/src/main/java/io/github/kwatera_project/kwatera/auth_service/model/User.java
@@ -1,12 +1,10 @@
 package io.github.kwatera_project.kwatera.auth_service.model;
 
 import jakarta.persistence.*;
-
 import java.time.Instant;
 import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -23,57 +21,57 @@ import org.springframework.security.core.userdetails.UserDetails;
 @Setter
 @NoArgsConstructor
 @EntityListeners(
-        AuditingEntityListener.class) // Enables auto-completion of createdAt, updatedAt fields.
+    AuditingEntityListener.class) // Enables auto-completion of createdAt, updatedAt fields.
 public class User implements UserDetails {
-    /// A class representing the user table in the database.
-    @Id // Master key.
-    @GeneratedValue(strategy = GenerationType.UUID) // Automatically generates UUID.
-    private UUID id;
+  /// A class representing the user table in the database.
+  @Id // Master key.
+  @GeneratedValue(strategy = GenerationType.UUID) // Automatically generates UUID.
+  private UUID id;
 
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING) // Saved in the database as text.
-    private Role role;
+  @Column(nullable = false)
+  @Enumerated(EnumType.STRING) // Saved in the database as text.
+  private Role role;
 
-    @Column(nullable = false, unique = true)
-    private String username;
+  @Column(nullable = false, unique = true)
+  private String username;
 
-    @Column(nullable = false, unique = true)
-    private String email;
+  @Column(nullable = false, unique = true)
+  private String email;
 
-    @Column(nullable = false)
-    private String password;
+  @Column(nullable = false)
+  private String password;
 
-    @Column(name = "created_at", nullable = false, updatable = false)
-    @CreatedDate // The field is set automatically when the record is created and cannot be changed
-    // later.
-    private Instant createdAt;
+  @Column(name = "created_at", nullable = false, updatable = false)
+  @CreatedDate // The field is set automatically when the record is created and cannot be changed
+  // later.
+  private Instant createdAt;
 
-    @Column(name = "updated_at")
-    @LastModifiedDate // Field updated automatically whenever a record changes.
-    private Instant updatedAt;
+  @Column(name = "updated_at")
+  @LastModifiedDate // Field updated automatically whenever a record changes.
+  private Instant updatedAt;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
-    }
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+  }
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
 
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
 
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
 
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
