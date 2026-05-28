377e19d4fef5f0a3d68de355f3f63268cd2f1a2a
alicjaswiers <alicjaswiers@wp.pl>
2026-05-28 20:03:10 +0200

    Format: inline empty CacheConfig & tidy apps

--- services/billing-service/src/main/java/io/github/kwatera_project/kwatera/billing_service/BillingServiceApplication.java
+++ services/billing-service/src/main/java/io/github/kwatera_project/kwatera/billing_service/BillingServiceApplication.java
@@ -2,6 +2,7 @@ package io.github.kwatera_project.kwatera.billing_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class BillingServiceApplication {
 
--- services/property-service/src/main/java/io/github/kwatera_project/kwatera/property_service/PropertyServiceApplication.java
+++ services/property-service/src/main/java/io/github/kwatera_project/kwatera/property_service/PropertyServiceApplication.java
@@ -2,6 +2,7 @@ package io.github.kwatera_project.kwatera.property_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class PropertyServiceApplication {
 
