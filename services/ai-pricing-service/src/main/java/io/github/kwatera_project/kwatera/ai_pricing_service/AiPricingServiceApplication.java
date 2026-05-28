c9fc19fd0d56179920a39c3cd2b933508ee0ffc3
Nadzeya Silchankava <sinadzeya@Nadzeyas-MacBook-Pro-2.local>
2026-05-28 10:21:33 +0200

    Add ai-pricing-service

---
+++ services/ai-pricing-service/src/main/java/io/github/kwatera_project/kwatera/ai_pricing_service/AiPricingServiceApplication.java
@@ -0,0 +1,12 @@
+package io.github.kwatera_project.kwatera.ai_pricing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPricingServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AiPricingServiceApplication.class, args);
+  }
+}
---
+++ services/ai-pricing-service/src/test/java/io/github/kwatera_project/kwatera/ai_pricing_service/AiPricingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kwatera_project.kwatera.ai_pricing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureTestDatabase
+class AiPricingServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
