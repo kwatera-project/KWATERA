4d8c7007b313c4c97fc8522823b71d79a8be837c
alicjaswiers <alicjaswiers@wp.pl>
2026-06-08 18:57:42 +0200

    Enable Kafka and exclude UserDetailsService

--- services/reservation-service/src/main/java/io/github/kwatera_project/kwatera/reservation_service/ReservationServiceApplication.java
+++ services/reservation-service/src/main/java/io/github/kwatera_project/kwatera/reservation_service/ReservationServiceApplication.java
@@ -2,10 +2,13 @@ package io.github.kwatera_project.kwatera.reservation_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@EnableKafka
 @EnableScheduling
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class ReservationServiceApplication {
 
   public static void main(String[] args) {
