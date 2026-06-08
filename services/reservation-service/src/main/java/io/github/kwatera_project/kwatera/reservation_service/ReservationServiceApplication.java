f85c1d2372a0dcbc8e0afa4a5dca4fd4773e69fb
Alicja Świercz <alicjaswiers@wp.pl>
2026-06-08 13:12:12 +0200

    Remove Kafka and security-exclude annotations

--- services/reservation-service/src/main/java/io/github/kwatera_project/kwatera/reservation_service/ReservationServiceApplication.java
+++ services/reservation-service/src/main/java/io/github/kwatera_project/kwatera/reservation_service/ReservationServiceApplication.java
@@ -2,13 +2,10 @@ package io.github.kwatera_project.kwatera.reservation_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableKafka
+@SpringBootApplication
 @EnableScheduling
-@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class ReservationServiceApplication {
 
   public static void main(String[] args) {
