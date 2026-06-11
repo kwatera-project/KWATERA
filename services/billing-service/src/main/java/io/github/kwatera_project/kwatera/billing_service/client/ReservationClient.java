78719d0c0ee2fd0ceef3a1760e901fcb56aecd03
Zuzanna Adamczyk <zuziadamczyk@wp.pl>
2026-06-11 12:40:20 +0200

    Fix ReservationClient to use internal token endpoint and mock in tests

--- services/billing-service/src/main/java/io/github/kwatera_project/kwatera/billing_service/client/ReservationClient.java
+++ services/billing-service/src/main/java/io/github/kwatera_project/kwatera/billing_service/client/ReservationClient.java
@@ -3,6 +3,10 @@ package io.github.kwatera_project.kwatera.billing_service.client;
 import io.github.kwatera_project.kwatera.billing_service.dto.ReservationDto;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,8 +16,18 @@ public class ReservationClient {
 
   private final RestTemplate restTemplate;
 
+  @Value("${kwatera.security.internal-token:kwatera-internal-secret-token}")
+  private String internalToken;
+
   public ReservationDto getReservation(UUID reservationId) {
-    String url = "http://reservation-service/api/v1/reservations/" + reservationId;
-    return restTemplate.getForObject(url, ReservationDto.class);
+
+    String url = "http://reservation-service/api/v1/reservations/internal/" + reservationId;
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.set("X-Internal-Token", internalToken);
+
+    HttpEntity<Void> entity = new HttpEntity<>(headers);
+
+    return restTemplate.exchange(url, HttpMethod.GET, entity, ReservationDto.class).getBody();
   }
 }
